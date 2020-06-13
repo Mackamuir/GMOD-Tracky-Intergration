d2cdc2af3054fb40ffef0293f277527b81ccb5d3
Mackamuir <mackamuir@gmail.com>
2020-06-13 20:21:57 +1000

    Update trackyloot.lua
    Fixed and finished the script

--- lua/autorun/trackyloot.lua
+++ lua/autorun/trackyloot.lua
@@ -3,22 +3,25 @@
 
 if SERVER then
 
-	util.AddNetworkString( "claim" )
-	util.AddNetworkString( "claimed" )
-	util.AddNetworkString( "fuckoff" )
+	util.AddNetworkString( "claim" );
+	util.AddNetworkString( "claimed" );
+	util.AddNetworkString( "fuckoff" );
+	util.AddNetworkString( "vote" );
+
+	CreateConVar("tracky_key", "d34a0aa6fe61458d13eda9afde9c25a2");
 
 	hook.Add( "PlayerSay", "CommandIndent", function( ply, text )
 
-		local stmid = ply:SteamID()
+		local stmid = ply:SteamID64()
 		local claim = ""
 
 		if string.lower(text) == "/claim" then
-		local url = string.Replace( "http://www.api.trackyserver.com/vote/?action=claim&key=152855&steamid=stmid", "stmid", stmid )
+		local url1 = string.Replace( "http://www.api.trackyserver.com/vote/?action=claim&key=trackkey&steamid=stmid", "stmid", stmid )
+		local url = string.Replace( url1, "trackkey", GetConVar( "tracky_key" ):GetString() )
 
 				http.Fetch( url,
 					function( body )
 					claim = body
-					print( claim )
 					--this is a fucked way to do this but its easy and works
 					if claim == "0" then
 						net.Start("claim")
@@ -37,18 +40,28 @@ if SERVER then
 			 		print( "Tracky Servers are probs fucked" )
 				end
 			)
+		elseif string.lower(text) == "/vote" then
+			print(vote)
+			net.Start("vote")
+			net.Send(ply)
 		end
 	end)
 end
 
 if CLIENT then
+	CreateConVar("tracky_id", "556920", FCVAR_PROTECTED);
+
 	net.Receive("claim",function(len)
-	chat.AddText( Color( 50, 237, 225 ), "[DG] ", Color( 255, 255, 255 ), "You haven't voted! Type /vote to get the voting link!")
+	chat.AddText( Color( 50, 237, 225 ), "[Tracky] ", Color( 255, 255, 255 ), "You haven't voted! Type /vote to get the voting link!")
 end)
 	net.Receive("claimed",function(len)
-	chat.AddText( Color( 50, 237, 225 ), "[DG] ", Color( 255, 255, 255 ), "Thanks for voting! Here is your reward.")
+	chat.AddText( Color( 50, 237, 225 ), "[Tracky] ", Color( 255, 255, 255 ), "Thanks for voting! Here is your reward.")
 end)
 	net.Receive("fuckoff",function(len)
-	chat.AddText( Color( 50, 237, 225 ), "[DG] ", Color( 255, 255, 255 ), "You already claimed your reward in the past 24 hours!")
+	chat.AddText( Color( 50, 237, 225 ), "[Tracky] ", Color( 255, 255, 255 ), "You already claimed your daily reward!")
 end)
+	net.Receive("vote",function(len)
+	chat.AddText( Color( 50, 237, 225 ), "[Tracky] ", Color( 255, 255, 255 ), string.Replace( "https://www.trackyserver.com/server/trackid", "trackid", GetConVar( "tracky_id" ):GetString() ) )
+end)
+
 end
